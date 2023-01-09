b4b7edc22c0f104b9c820a4cdd191ff4a6da5429
Kiran <kiranraghuveer2500@gmail.com>
2023-01-09 17:05:58 +0530

    Inital Commit

---
+++ src/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [userName, setUserName] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userName);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="userName">username</label>
+                <input value={userName} onChange={(e) => setUserName(e.target.value)}type="userName" placeholder="your name" id="email" name="userName" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
